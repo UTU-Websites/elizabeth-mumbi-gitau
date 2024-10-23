2772985bf0ad595315c6dd6c2e18b7ab4b9d4c1a
S-Qui <147270652+S-Qui@users.noreply.github.com>
2024-10-23 17:04:26 +0300

    retouch
    font in active menu
    font in body div - weight and size

--- src/calender.js
+++ src/calender.js
@@ -30,19 +30,19 @@ const monthsOfYear = ["January", "February", "March", "April", "May", "June", "J
 
 // Event data
 const events = [
-  { day: 3, date: '2024-10-23', title: "Macakaya ~ Wake Service", time: "04:00 PM", location: "Gitau Family Residence" },
+  { day: 3, date: '2024-10-23', title: "Macakaya ~ Wake", time: "04:00 PM", location: "Gitau Family Residence" },
   { day: 3, date: '2024-10-23', title: "Committee Meeting", time: "07:00 PM", location: "Gitau Family Residence" },
-  { day: 4, date: '2024-10-24', title: "Macakaya ~ Wake Service", time: "04:00 PM", location: "Gitau Family Residence" },
+  { day: 4, date: '2024-10-24', title: "Macakaya ~ Wake", time: "04:00 PM", location: "Gitau Family Residence" },
   { day: 4, date: '2024-10-24', title: "Committee Meeting", time: "07:00 PM", location: "Gitau Family Residence" },
-  { day: 5, date: '2024-10-25', title: "Macakaya ~ Wake Service", time: "04:00 PM", location: "Gitau Family Residence" },
+  { day: 5, date: '2024-10-25', title: "Macakaya ~ Wake", time: "04:00 PM", location: "Gitau Family Residence" },
   { day: 5, date: '2024-10-25', title: "Committee Meeting", time: "07:00 PM", location: "Gitau Family Residence" },
-  { day: 6, date: '2024-10-26', title: "Macakaya ~ Wake Service", time: "04:00 PM", location: "Gitau Family Residence" },
+  { day: 6, date: '2024-10-26', title: "Macakaya ~ Wake", time: "04:00 PM", location: "Gitau Family Residence" },
   { day: 6, date: '2024-10-26', title: "Committee Meeting", time: "07:00 PM", location: "Gitau Family Residence" },
-  { day: 0, date: '2024-10-27', title: "Macakaya ~ Wake Service", time: "04:00 PM", location: "Gitau Family Residence" },
+  { day: 0, date: '2024-10-27', title: "Macakaya ~ Wake", time: "04:00 PM", location: "Gitau Family Residence" },
   { day: 0, date: '2024-10-27', title: "Committee Meeting", time: "07:00 PM", location: "Gitau Family Residence" },
-  { day: 1, date: '2024-10-28', title: "Macakaya ~ Wake Service", time: "04:00 PM", location: "Gitau Family Residence" },
+  { day: 1, date: '2024-10-28', title: "Macakaya ~ Wake", time: "04:00 PM", location: "Gitau Family Residence" },
   { day: 1, date: '2024-10-28', title: "Committee Meeting", time: "07:00 PM", location: "Gitau Family Residence" },
-  { day: 2, date: '2024-10-29', title: "Macakaya ~ Wake Service", time: "04:00 PM", location: "Gitau Family Residence" },
+  { day: 2, date: '2024-10-29', title: "Macakaya ~ Wake", time: "04:00 PM", location: "Gitau Family Residence" },
   { day: 2, date: '2024-10-29', title: "Committee Meeting", time: "07:00 PM", location: "Gitau Family Residence" },
   { day: 3, date: '2024-10-30', title: "FUNERAL SERVICE", time: "08:00 AM", location: "P.C.E.A Kihumo Church"}
 ];

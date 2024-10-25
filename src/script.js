37a978e44ce5c9792cefbce53ffa9bb3a8ba5f98
S-Qui <147270652+S-Qui@users.noreply.github.com>
2024-10-25 23:37:42 +0300

    pbnjToast sec/ replace img with link

--- src/script.js
+++ src/script.js
@@ -28,7 +28,7 @@ window.addEventListener('scroll', function () {
 //alert toast
 document.addEventListener('DOMContentLoaded', function () {
   var pbnjToast = new bootstrap.Toast(document.getElementById('pbnjToast'), {
-    delay: 15000  // 5 seconds
+    delay: 7000 //seconds
   });
   pbnjToast.show();
 });
